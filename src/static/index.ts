ba695812e3e1bc4e599c2db94e803199a4107238
o-henry <cksgod16@gmail.com>
2020-05-30 10:43:32 +0900

    Add image

--- src/static/index.ts
+++ src/static/index.ts
@@ -1,1 +1,15 @@
+/* project Image */
+export { default as blink } from "./blink.png";
+export { default as blinker } from "./blinker.png";
+export { default as blinkerImg } from "./blinkerImg.png";
+export { default as cookie } from "./cookie.png";
+export { default as crunch } from "./crunch.png";
+export { default as local } from "./local.png";
+export { default as localsns } from "./localsns.png";
+export { default as papaya } from "./papaya.png";
+export { default as papayaland } from "./papayaland.png";
+export { default as papayaweb } from "./papayaweb.png";
+export { default as papayaweb2 } from "./papayaweb2.png";
+
+/* asset */
 export { default as profile } from "./profile.svg";
